b987b8cda06902e4ca86ca56e0c935686f888d0e
emanul <emnaueljose0522@gmail.com>
2025-11-14 07:53:01 -0500

    configuered the DbContext and started the suthentication service and controllers

---
+++ MarketHub_API/MarketHub_API/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketHub_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        public AuthController()
+        {
+        }
+    }
+}
---
+++ MarketHub_API/MarketHub_API/Services/IAuthService.cs
@@ -0,0 +1,46 @@
+﻿using MarketHub_API.Data.MarketHub_API.Data;
+using MarketHub_API.Models;
+
+namespace MarketHub_API.Services
+{
+    public interface IAuthService
+    {
+        Task<User> RegisterAsync(string email, string password, string firstNaem, string lastNaem, string phone);
+        Task<string> LoginAsync(string email, string password);
+        string HashPassword(string password);
+        bool VerifyPassword(string password, string passwordHash);
+    }
+
+    public class AuthService : IAuthService
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public AuthService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public string HashPassword(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> LoginAsync(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> RegisterAsync(string email, string password, string firstNaem, string lastNaem, string phone)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool VerifyPassword(string password, string passwordHash)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
